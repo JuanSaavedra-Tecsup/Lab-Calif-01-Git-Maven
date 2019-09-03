e31382dc9955500f71746d43a4787a33d122df94
JuanSaavedra-Tecsup <juan.saavedra.p@tecsup.edu.pe>
2019-09-03 17:08:47 -0500

    my first commit

--- src/main/java/com/tecsup/lab01/Controller/UserController.java
+++ src/main/java/com/tecsup/lab01/Controller/UserController.java
@@ -1,5 +1,13 @@
 package com.tecsup.lab01.Controller;
 
-public class UserController {
+import javax.swing.JOptionPane;
+
+public class UserController extends com.tecsup.lab01.Model.User {
+    public void registrar(){
+        nombre = JOptionPane.showInputDialog("Ingrese su Nombre");
+        apellidos = JOptionPane.showInputDialog("Ingrese su Apellido Paterno");
+        edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su Edad"));
+    }
+
 
 }
--- src/main/java/com/tecsup/lab01/Model/User.java
+++ src/main/java/com/tecsup/lab01/Model/User.java
@@ -1,5 +1,8 @@
 package com.tecsup.lab01.Model;
 
 public class User {
+    protected static String nombre;
+    protected static String apellidos;
+    protected static int edad;
 
 }
